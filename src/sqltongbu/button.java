7f9d1cb79162c0a52ce9f06c560087ab8e3a27cf
Database synchronization <1805418518@qq.com>
2019-12-05 12:08:46 +0800

    2019/12/5
    12：08

--- src/sqltongbu/button.java
+++ src/sqltongbu/button.java
@@ -16,7 +16,44 @@ public class button {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-
+			JFrame jf=new JFrame("±‡º≠¡¨Ω”");
+			jf.setLayout(null);
+			
+			JLabel jl_address=new JLabel("µÿ÷∑");
+			JLabel jl_user=new JLabel("’À∫≈");
+			JLabel jl_password=new  JLabel("√‹¬Î");
+			
+			JTextField jt_addres=new JTextField();
+			JTextField jt_user=new JTextField();
+			JPasswordField jp_password=new JPasswordField();
+			
+			JButton jb_submission=new JButton("Ã·Ωª");
+			JButton jb_test=new JButton("≤‚ ‘");
+			
+			jl_address.setBounds(50, 50, 100, 25);
+			jl_user.setBounds(50, 85, 100, 25);
+			jl_password.setBounds(50, 120, 100, 25);
+			
+			
+			jt_addres.setBounds(100, 50, 150, 25);
+			jt_user.setBounds(100, 85, 150, 25);
+			jp_password.setBounds(100, 120, 150, 25);
+			
+			jb_submission.setBounds(50, 200, 80, 25);
+			jb_test.setBounds(140, 200, 80, 25);
+			
+			
+			jf.add(jl_address);
+			jf.add(jl_user);
+			jf.add(jl_password);
+			jf.add(jt_addres);
+			jf.add(jt_user);
+			jf.add(jp_password);
+			jf.add(jb_submission);
+			jf.add(jb_test);
+			
+			jf.setSize(300, 300);
+			jf.setVisible(true);
 		}
 
 	}
@@ -26,7 +63,44 @@ public class button {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-
+			JFrame jf=new JFrame("±‡º≠¡¨Ω”");
+			jf.setLayout(null);
+			
+			JLabel jl_address=new JLabel("µÿ÷∑");
+			JLabel jl_user=new JLabel("’À∫≈");
+			JLabel jl_password=new  JLabel("√‹¬Î");
+			
+			JTextField jt_addres=new JTextField();
+			JTextField jt_user=new JTextField();
+			JPasswordField jp_password=new JPasswordField();
+			
+			JButton jb_submission=new JButton("Ã·Ωª");
+			JButton jb_test=new JButton("≤‚ ‘");
+			
+			jl_address.setBounds(50, 50, 100, 25);
+			jl_user.setBounds(50, 85, 100, 25);
+			jl_password.setBounds(50, 120, 100, 25);
+			
+			
+			jt_addres.setBounds(100, 50, 150, 25);
+			jt_user.setBounds(100, 85, 150, 25);
+			jp_password.setBounds(100, 120, 150, 25);
+			
+			jb_submission.setBounds(50, 200, 80, 25);
+			jb_test.setBounds(140, 200, 80, 25);
+			
+			
+			jf.add(jl_address);
+			jf.add(jl_user);
+			jf.add(jl_password);
+			jf.add(jt_addres);
+			jf.add(jt_user);
+			jf.add(jp_password);
+			jf.add(jb_submission);
+			jf.add(jb_test);
+			
+			jf.setSize(300, 300);
+			jf.setVisible(true);
 		}
 
 	}
@@ -36,7 +110,44 @@ public class button {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
-
+			JFrame jf=new JFrame("±‡º≠¡¨Ω”");
+			jf.setLayout(null);
+			
+			JLabel jl_address=new JLabel("µÿ÷∑");
+			JLabel jl_user=new JLabel("’À∫≈");
+			JLabel jl_password=new  JLabel("√‹¬Î");
+			
+			JTextField jt_addres=new JTextField();
+			JTextField jt_user=new JTextField();
+			JPasswordField jp_password=new JPasswordField();
+			
+			JButton jb_submission=new JButton("Ã·Ωª");
+			JButton jb_test=new JButton("≤‚ ‘");
+			
+			jl_address.setBounds(50, 50, 100, 25);
+			jl_user.setBounds(50, 85, 100, 25);
+			jl_password.setBounds(50, 120, 100, 25);
+			
+			
+			jt_addres.setBounds(100, 50, 150, 25);
+			jt_user.setBounds(100, 85, 150, 25);
+			jp_password.setBounds(100, 120, 150, 25);
+			
+			jb_submission.setBounds(50, 200, 80, 25);
+			jb_test.setBounds(140, 200, 80, 25);
+			
+			
+			jf.add(jl_address);
+			jf.add(jl_user);
+			jf.add(jl_password);
+			jf.add(jt_addres);
+			jf.add(jt_user);
+			jf.add(jp_password);
+			jf.add(jb_submission);
+			jf.add(jb_test);
+			
+			jf.setSize(300, 300);
+			jf.setVisible(true);
 		}
 
 	}
