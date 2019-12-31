d54b69d3db7104b7a0ad86e7a4a7e0c1cad929e5
gc1175383875 <gc1998gc@qq.com>
2020-01-01 01:12:23 +0800

    数据库链接测试

--- src/Util/DBUtil2.java
+++ src/Util/DBUtil2.java
@@ -7,7 +7,7 @@ public class DBUtil2 {
 	public static Connection   getConnection(String ac_user,String ac_password,String ac_surface,String ac_address) {
 		 String driver = "oracle.jdbc.driver.OracleDriver";//oracle驱动
 		 String url = "jdbc:oracle:thin:@"+ ac_address+":1521:"+ac_surface;//ip:1521:xe
-		 String username = ac_user;//数据库用户名 as 连接为（有Normal,SYSDBA,SYSOPER）
+		 String username = ac_user +" as "+ "SYSDBA";//数据库用户名 as 连接为（有Normal,SYSDBA,SYSOPER）
 		 String password =ac_password ;//密码
 		Connection connection=null;
 		
